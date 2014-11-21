e45d6a9fd1bb930ede1d4daf75fd81c91ecaaf96
Adam Krone <krone.adam@gmail.com>
2014-11-21 16:10:10 -0600

    Extracts option parsing out of main

--- main.go
+++ main.go
@@ -6,75 +6,85 @@ import (
 	"os"
 )
 
-func main() {
-	var configFile string
-	var host string
-	var user string
-	var password string
-	var action string
-	var realm string
-	var group_id string
-	var role_id string
-	var node string
-	var upid string
-	var vmid string
-	var ostemplate string
-	var cpus int
-	var disk int
-	var hostname string
-	var ip_address string
-	var memory int
-	var swap int
+type Options struct {
+	configFile string
+	host       string
+	user       string
+	password   string
+	action     string
+	realm      string
+	group_id   string
+	role_id    string
+	node       string
+	upid       string
+	vmid       string
+	ostemplate string
+	cpus       int
+	disk       int
+	hostname   string
+	ip_address string
+	memory     int
+	swap       int
+}
 
-	flag.StringVar(&configFile, "config", "", "Proxmox config file")
-	flag.StringVar(&host, "host", "", "Proxmox host")
-	flag.StringVar(&user, "user", "root@pam", "Proxmox user")
-	flag.StringVar(&password, "password", "", "Proxmox user password")
-	flag.StringVar(&action, "action", "", "Proxmox api action")
-	flag.StringVar(&realm, "realm", "pve", "Proxmox realm")
-	flag.StringVar(&group_id, "group-id", "", "Proxmox group")
-	flag.StringVar(&role_id, "role-id", "", "Proxmox role")
-	flag.StringVar(&node, "node", "", "Proxmox node")
-	flag.StringVar(&upid, "upid", "", "Proxmox task UPID")
-	flag.StringVar(&vmid, "vmid", "", "OpenVZ container VMID")
-	flag.StringVar(&ostemplate, "ostemplate", "", "OpenVZ container template")
-	flag.IntVar(&cpus, "cpus", 0, "Number of CPUs")
-	flag.IntVar(&disk, "disk", 0, "Disk size")
-	flag.StringVar(&hostname, "hostname", "", "Hostname")
-	flag.StringVar(&ip_address, "ip-address", "", "IP Address")
-	flag.IntVar(&memory, "memory", 0, "Memory")
-	flag.IntVar(&swap, "swap", 0, "Swap")
+func getOpts() Options {
+	options := Options{}
+
+	flag.StringVar(&options.configFile, "config", "", "Proxmox config file")
+	flag.StringVar(&options.host, "host", "", "Proxmox host")
+	flag.StringVar(&options.user, "user", "root@pam", "Proxmox user")
+	flag.StringVar(&options.password, "password", "", "Proxmox user password")
+	flag.StringVar(&options.action, "action", "", "Proxmox api action")
+	flag.StringVar(&options.realm, "realm", "pve", "Proxmox realm")
+	flag.StringVar(&options.group_id, "group-id", "", "Proxmox group")
+	flag.StringVar(&options.role_id, "role-id", "", "Proxmox role")
+	flag.StringVar(&options.node, "node", "", "Proxmox node")
+	flag.StringVar(&options.upid, "upid", "", "Proxmox task UPID")
+	flag.StringVar(&options.vmid, "vmid", "", "OpenVZ container VMID")
+	flag.StringVar(&options.ostemplate, "ostemplate", "", "OpenVZ container template")
+	flag.IntVar(&options.cpus, "cpus", 0, "Number of CPUs")
+	flag.IntVar(&options.disk, "disk", 0, "Disk size")
+	flag.StringVar(&options.hostname, "hostname", "", "Hostname")
+	flag.StringVar(&options.ip_address, "ip-address", "", "IP Address")
+	flag.IntVar(&options.memory, "memory", 0, "Memory")
+	flag.IntVar(&options.swap, "swap", 0, "Swap")
 
 	flag.Parse()
 
-	config := ReadProxmoxConfig(configFile)
+	return options
+}
 
-	if config.Host != "" && host == "" {
-		host = config.Host
+func main() {
+	options := getOpts()
+
+	config := ReadProxmoxConfig(options.configFile)
+
+	if config.Host != "" && options.host == "" {
+		options.host = config.Host
 	}
-	if config.User != "" && user == "" {
-		user = config.User
+	if config.User != "" && options.user == "" {
+		options.user = config.User
 	}
-	if config.Password != "" && password == "" {
-		password = config.Password
+	if config.Password != "" && options.password == "" {
+		options.password = config.Password
 	}
-	if config.DefaultNode != "" && node == "" {
-		node = config.DefaultNode
+	if config.DefaultNode != "" && options.node == "" {
+		options.node = config.DefaultNode
 	}
 
 	proxmox := Proxmox{}
-	proxmox.host = host
-	proxmox.user = user
-	proxmox.password = password
+	proxmox.host = options.host
+	proxmox.user = options.user
+	proxmox.password = options.password
 	proxmox.auth = proxmox.GetAuth()
 
-	switch action {
+	switch options.action {
 	case "getDomains":
 		domains := proxmox.GetDomains()
 		PrintDataSlice(domains)
 	case "getRealmConfig":
 		domain := Domain{}
-		domain.Realm = realm
+		domain.Realm = options.realm
 		config := proxmox.GetRealmConfig(domain)
 		PrintDataStruct(config)
 	case "getGroups":
@@ -82,19 +92,19 @@ func main() {
 		PrintDataSlice(groups)
 	case "getGroupConfig":
 		var group Group
-		group.GroupId = group_id
+		group.GroupId = options.group_id
 		config := proxmox.GetGroupConfig(group)
 		PrintDataStruct(config)
 	case "createGroup":
 		var group Group
-		group.GroupId = group_id
+		group.GroupId = options.group_id
 		proxmox.CreateGroup(group)
 	case "getRoles":
 		roles := proxmox.GetRoles()
 		PrintDataSlice(roles)
 	case "getRoleConfig":
 		var role Role
-		role.RoleId = role_id
+		role.RoleId = options.role_id
 		config := proxmox.GetRoleConfig(role)
 		PrintDataStruct(config)
 	case "getClusterStatus":
@@ -111,29 +121,29 @@ func main() {
 		PrintDataSlice(nodes)
 	case "getNodeTaskStatus":
 		request := NodeTaskStatusRequest{}
-		request.Node = node
-		request.UPID = upid
+		request.Node = options.node
+		request.UPID = options.upid
 		status := proxmox.GetNodeTaskStatus(request)
 		PrintDataStruct(status)
 	case "getContainers":
-		proxmox.node = node
+		proxmox.node = options.node
 		containers := proxmox.GetContainers()
 		PrintDataSlice(containers)
 	case "getContainerConfig":
 		var req = ContainerRequest{}
-		req.Node = node
-		req.VMID = vmid
+		req.Node = options.node
+		req.VMID = options.vmid
 		containerConfig := proxmox.GetContainerConfig(req)
 		PrintDataStruct(containerConfig)
 	case "createContainer":
 		req := &ContainerRequest{}
-		req.Node = node
-		req.VMID = vmid
-		req.OsTemplate = ostemplate
+		req.Node = options.node
+		req.VMID = options.vmid
+		req.OsTemplate = options.ostemplate
 		fmt.Printf("Creating container")
 		upid := proxmox.CreateContainer(req)
 		statusRequest := NodeTaskStatusRequest{}
-		statusRequest.Node = node
+		statusRequest.Node = options.node
 		statusRequest.UPID = upid
 		task := proxmox.CheckNodeTaskStatus(statusRequest)
 		if task.ExitStatus == "OK" {
@@ -143,12 +153,12 @@ func main() {
 		}
 	case "deleteContainer":
 		request := &ContainerRequest{}
-		request.Node = node
-		request.VMID = vmid
+		request.Node = options.node
+		request.VMID = options.vmid
 		fmt.Printf("Deleting container")
 		upid := proxmox.DeleteContainer(request)
 		statusRequest := NodeTaskStatusRequest{}
-		statusRequest.Node = node
+		statusRequest.Node = options.node
 		statusRequest.UPID = upid
 		task := proxmox.CheckNodeTaskStatus(statusRequest)
 		if task.ExitStatus == "OK" {
@@ -157,7 +167,7 @@ func main() {
 			fmt.Println("Exit Status: %s", task.ExitStatus)
 		}
 	default:
-		fmt.Printf("Unsupported action: %s", action)
+		fmt.Printf("Unsupported action: %s", options.action)
 		os.Exit(1)
 	}
 }
